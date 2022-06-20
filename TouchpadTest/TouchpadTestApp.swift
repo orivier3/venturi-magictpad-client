d50f0cebc4433661653d48e137e26b6bad70568a
Olivier Riviere <oriviere@logitech.com>
2022-06-20 10:08:50 +0100

    Initial Commit

---
+++ TouchpadTest/TouchpadTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchpadTestApp.swift
+//  TouchpadTest
+//
+//  Created by Olivier Riviere on 20/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct TouchpadTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
